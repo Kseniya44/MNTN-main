b92ae705c4082dd5cbd057853c994db2832dc590
kseniya <kseniya.for.email@gmail.com>
2022-04-17 18:10:22 +0300

    Section hero was added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ const openMenuIcon = document.getElementById("menu-toggle-open");
 const closeMenuIcon = document.getElementById("menu-toggle-close");
 const mobileMenu = document.getElementById("mobile-menu");
 const headerOverlay = document.getElementById("header");
+const grassImage = document.getElementById("grassland");
+const mountainsImage = document.getElementById("mountains");
+const skyImage = document.getElementById("sky");
+const heroData = document.getElementById("hero-data");
 
 // Open menu & add overlay
 
@@ -15,6 +19,14 @@ openMenuIcon.addEventListener("click", () => {
 closeMenuIcon.addEventListener("click", () => {
     mobileMenu.classList.remove("menu-opened");
     headerOverlay.classList.remove('menu-opened');
+});
+
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY;
+    grassImage.style.transform = "translateY(" + scrollPosition * 0.02 + "%)";
+    mountainsImage.style.transform = "translateY(" + scrollPosition * 0.04 + "%)";
+    skyImage.style.transform = "translateY(" + scrollPosition * 0.01 + "%)";
+    heroData.style.opacity = 1 - scrollPosition * 0.002;
 });
 
 // Close menu & remove overlay
